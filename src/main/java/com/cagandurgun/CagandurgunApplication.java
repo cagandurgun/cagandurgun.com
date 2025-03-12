1a3ec28992091cc7f707fe69e2531c48be341c34
Çağan Durgun <cagandurgun@Cagan-Mac-mini.local>
2025-03-12 22:19:17 +0300

    first

---
+++ src/main/java/com/cagandurgun/CagandurgunApplication.java
@@ -0,0 +1,14 @@
+package com.cagandurgun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CagandurgunApplication {
+
+	// http://localhost:8080
+	public static void main(String[] args) {
+		SpringApplication.run(CagandurgunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cagandurgun/CagandurgunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagandurgun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CagandurgunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
